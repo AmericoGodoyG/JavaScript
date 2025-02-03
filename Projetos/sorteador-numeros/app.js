4a68e0d88bb349fd3f727eb1faa5c345443ac4fd
Américo Godoy da Silva <160660569+AmericoGodoyG@users.noreply.github.com>
2025-02-03 15:36:51 -0300

    Add files via upload

---
+++ Projetos/sorteador-numeros/app.js
@@ -0,0 +1,61 @@
+function sortear() {
+    // Recuperando elementos no html atrvés do id
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+    for(let i = 0; i < quantidade; i++) {
+
+        numero = obterNumeroAleatorio(de, ate);
+        
+        // Includes verifica se um elemento esta presente no array
+        while(sorteados.includes(numero)) {
+            
+            numero = obterNumeroAleatorio(de, ate);
+            
+        };
+        
+        // push adiciona um elemento no fim do array
+        sorteados.push(numero);
+    };
+
+    // Exbindo uma informação no html
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`;
+
+    alterarStatusBotao();
+};
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+function alterarStatusBotao () {
+
+    let botao =  document.getElementById('btn-reiniciar');
+
+    if (botao.classList.contains('container__botao-desabilitado')) {
+
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+
+    } else {
+
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+
+    };
+};
+
+function reiniciar() {
+
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+    
+};
