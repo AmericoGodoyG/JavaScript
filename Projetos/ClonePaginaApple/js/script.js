b17cda60c67435c3ba25d80ebcb458022ee3e770
Américo Godoy da Silva <160660569+AmericoGodoyG@users.noreply.github.com>
2025-02-21 10:22:54 -0300

    Add files via upload

---
+++ Projetos/ClonePaginaApple/js/script.js
@@ -0,0 +1,27 @@
+// Selecionando os elementos
+const buttons = document.querySelectorAll('#image-picker li');
+const image = document.querySelector('product-image');
+
+// Criando um evento para cada um dos botões
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      console.log(e);
+  
+      buttons.forEach((btn) =>
+        btn.querySelector(".color").classList.remove("selected")
+      );
+  
+      const button = e.target;
+  
+      const id = button.getAttribute("id");
+  
+      button.querySelector(".color").classList.add("selected");
+  
+      image.classList.toggle("changing");
+      image.setAttribute("src", `img/iphone_${id}.jpg`);
+  
+      setTimeout(() => {
+        image.classList.toggle("changing");
+      }, 200);
+    });
+  });
